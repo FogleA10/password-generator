b4f0db1e1a6366c48db12390a869072b188741b9
audreyfogle <FogleA10@gmail.com>
2022-03-11 21:46:34 -0500

    set up for loop and clean up usersChars

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # password-generator
+
+
+html defaul to kebab case
+hello-im-in-kebab-case
+
+JS default to camel case
+camelCaseIsCool
+
+snake case
+snake_case_is_like_this
+
+Window Pop Ups
+- Alert - notifies user
+- Prompt - user inputs info
+- Confirm - true false
---
+++ script.js
@@ -0,0 +1,118 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Options  
+// alternative option to var lowerCase
+
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var lowerCase = ['a','b','c','d','e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r' ,'s','t','u','v','w','x','y', 'z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var specialCharacters = [ '.', '//', ':', ';', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+', '~', '`']
+
+function userInput() {
+  var length=window.prompt("How long would you like your password?");
+  
+  if(isNaN(length)) {
+    alert("The password length must be a number!!!")
+    return;
+  }
+
+  if(length<8 || length>128 ) {
+    alert("Password must be between 8 and 128 characters.")
+    return;
+  }
+
+  var useNumbers=confirm("Would you like your password to have numbers?")
+
+  var useUppercase=confirm("Would you like your password to have Uppercase letters?")
+
+  var useLowercase=confirm(" Would you like your password to have lowercase letters?")
+
+  var useSpecialCharacters=confirm(" Would like your password to have special characters?")
+
+  if(useNumbers===false&& useUppercase===false&& useLowercase===false&& useSpecialCharacters===false){
+    alert("MUST choose from at least one character option.")
+    return;
+  }
+
+  var userData = {
+    length: length,
+    useNumbers: useNumbers,
+    useUppercase: useUppercase,
+    useLowercase: useLowercase,
+    useSpecialCharacters: useSpecialCharacters
+
+  }
+
+  return userData;
+
+}
+
+function randomizer(array) {
+  //use math.floor(Math.random)
+  var index= Math.floor(Math.random()  *array.length) 
+  //randomly selecting index from the array, needs more, its only a bag 
+  //use the index to return that element of array
+  return array[index];
+}
+var randomLetter=randomizer (upperCase);
+var randomLetter=randomizer (lowerCase);
+var randomLetter=randomizer (specialCharacters);
+
+
+
+function generatePassword() {
+  var passwordOptions = userInput();
+
+  var result=[];
+  var usersChars = [];
+  var requiredChars=[];
+
+//Put password generate code here for each one
+//for loop at the end of this function
+
+if(passwordOptions.useNumbers ===true) {
+  usersChars = usersChars.concat(numbers);
+  requiredChars.push(randomizer(numbers))
+}
+
+if(passwordOptions.useLowercase===true) {
+  usersChars =usersChars.concat(lowerCase);
+  requiredChars.push(randomizer(lowerCase))
+}
+if(passwordOptions.useUppercase===true) {
+  usersChars = usersChars.concat(upperCase);
+  requiredChars.push(randomizer(upperCase))
+}
+if(passwordOptions.useSpecialCharacters===true){
+  usersChars = usersChars.concat(specialCharacters);
+  requiredChars.push(randomizer(specialCharacters))
+}
+for(var i=0; i<passwordOptions.length; i++){
+  randomizer(usersChars);
+
+  return(randomizer);
+}
+
+
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
