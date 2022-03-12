f46e0b04d3d7e1d0c1b367ba59670fa3d5e94bd6
audreyfogle <FogleA10@gmail.com>
2022-03-12 10:56:16 -0500

    adding result and finishing up randomValue in password generate

--- script.js
+++ script.js
@@ -50,10 +50,10 @@ function userInput() {
 
 function randomizer(array) {
   //use math.floor(Math.random)
-  var index= Math.floor(Math.random()  *array.length) 
+  var index = Math.floor(Math.random()  * array.length); 
   //randomly selecting index from the array, needs more, its only a bag 
   //use the index to return that element of array
-  return array[index];
+  return array[index]; // 0, f, $
 }
 var randomLetter=randomizer (upperCase);
 var randomLetter=randomizer (lowerCase);
@@ -64,36 +64,44 @@ var randomLetter=randomizer (specialCharacters);
 function generatePassword() {
   var passwordOptions = userInput();
 
-  var result=[];
+  // {
+  //   length: 8,
+  //   useNumbers: true,
+  //   useUppercase: false,
+  //   useLowercase: true,
+  //   useSpecialCharacters: true
+  // }
+
+  var result = "";
   var usersChars = [];
-  var requiredChars=[];
 
-//Put password generate code here for each one
-//for loop at the end of this function
+  //Put password generate code here for each one
+  //for loop at the end of this function
 
-if(passwordOptions.useNumbers ===true) {
-  usersChars = usersChars.concat(numbers);
-  requiredChars.push(randomizer(numbers))
-}
+  if(passwordOptions.useNumbers ===true) {
+    usersChars = usersChars.concat(numbers);
+  }
 
-if(passwordOptions.useLowercase===true) {
-  usersChars =usersChars.concat(lowerCase);
-  requiredChars.push(randomizer(lowerCase))
-}
-if(passwordOptions.useUppercase===true) {
-  usersChars = usersChars.concat(upperCase);
-  requiredChars.push(randomizer(upperCase))
-}
-if(passwordOptions.useSpecialCharacters===true){
-  usersChars = usersChars.concat(specialCharacters);
-  requiredChars.push(randomizer(specialCharacters))
-}
-for(var i=0; i<passwordOptions.length; i++){
-  randomizer(usersChars);
+  if(passwordOptions.useLowercase===true) {
+    usersChars =usersChars.concat(lowerCase);
+  }
+  
+  if(passwordOptions.useUppercase===true) {
+    usersChars = usersChars.concat(upperCase);
+  }
 
-  return(randomizer);
-}
+  if(passwordOptions.useSpecialCharacters===true){
+    usersChars = usersChars.concat(specialCharacters);
+  }
 
+  for(var i = 0; i < passwordOptions.length; i++){
+    var randomValue = randomizer(usersChars);
+
+    result = result + randomValue; // "" + "A"     "A" + "B"   "AB" + "C"
+
+  }
+  
+  return result;
 
 }
 
@@ -105,7 +113,6 @@ function writePassword() {
 
   passwordText.value = password;
   
-
 }
 
 // Add event listener to generate button
